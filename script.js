989d4888927df31714517bd701366156214a2457
AsMRV <asl8mv@gmail.com>
2024-11-09 13:17:03 +0500

    длбавил сущность заявки

--- script.js
+++ script.js
@@ -2,11 +2,24 @@
 const express = require("express");
 // создаем объект приложения
 const app = express();
+
+let Order = {
+    number: 1,
+    day: 11,
+    month: 9,
+    year: 2024,
+    device: "iPhone",
+    problem_type: "проблема с экраном",
+    description: "Не работает экран",
+    client: "Андрей",
+    status: "в работе",
+};
+
 // определяем обработчик для маршрута "/"
-app.get("/", function(request, response){
+app.get("/", function(request, response) {
      
     // отправляем ответ
-    response.send("<h2>Привет Express!</h2>");
+    response.send("Order");
 });
 // начинаем прослушивать подключения на 3000 порту
 app.listen(3000);
